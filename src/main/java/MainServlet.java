1aa99844d46d9e012cac2e87794dfec0ebe450d2
nik1998 <beregnovnikita@gmail.com>
2020-09-16 10:41:09 +0300

    Add session

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,9 +12,23 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+        String username = req.getParameter("username");
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Hello!");
+        if (username == null) {
+            printWriter.write("Hello, Anonymous" + "<br>");
+        } else {
+            printWriter.write("Hello, " + username + "<br>");
+        }
+        printWriter.write("Page was visited " + visitCounter + " times.");
         printWriter.close();
     }
 }
